596dfde9fb8ffb5ea7ce7e2a1fb106254a56e75b
JohnnyHu <karta1337366@gmail.com>
2022-12-22 15:44:53 +0800

    Co-authored-by: Lîm Tsú-thuàn <dannypsnl@gmail.com> Co-authored-by: Terry Chan <terrychan999@gmail.com>

--- static/js/decisionpath.js
+++ static/js/decisionpath.js
@@ -1,68 +1,64 @@
-  // Create a new directed graph
-  console.log('from decisionpath.js')
+// Create a new directed graph
+console.log('from decisionpath.js')
+var g = new dagreD3.graphlib.Graph().setGraph({});
+g.setNode("start", { label: "start" });
+g.setNode("infected", { label: "infected" });
 
-  var g = new dagreD3.graphlib.Graph().setGraph({});
+function insertEdge(g, na, nb, label) {
+    if (!g.hasEdge(na, nb)) {
+        g.setEdge(na, nb, label)
+    }
+}
+function insertNode(g, name) {
+    if (!g.hasNode(name)) {
+        g.setNode(name, { label: name })
+    }
+}
 
-  var nodes = [ "Start", "Histology", "Serum CRP", "PT",
-                 "Neutrophil Segment", "APTT", "Serum ESR", "Synovial WBC", "Infected"
-              ];
-  
-  // Automatically label each of the nodes
-  nodes.forEach(function(node) {
-      g.setNode(node, { label: node }); 
-  });
-  
-  //LV1
-  g.setEdge("Start", "Histology", { label: "" });
-  g.setEdge("Start", "APTT", { label: "" });
-  //LV2
-  g.setEdge("Histology", "Serum CRP", { label: "positive" });
-  g.setEdge("Histology", "PT", { label: "positive" });
-  g.setEdge("APTT", "Serum ESR", { label: "≥ 28.0" });
-  //LV3 
-  g.setEdge("Serum CRP", "Infected", { label: "≥ 16.0" });
-  g.setEdge("PT", "Neutrophil Segment", { label: "≥ 9.8" });
-  g.setEdge("Serum ESR", "Synovial WBC", { label: "≥ 38.0" });
-  //LV4
-  g.setEdge("Neutrophil Segment", "Infected", { label: "≥ 62.0" });
-  g.setEdge("Synovial WBC", "Infected", { label: "≥ 3780.0" });
-//   g.setEdge("148570025_1100", "148570010_1100", { label: "" });
-//   g.setEdge("148570025_1100", "148570026_1100", { label: "" });
-//   g.setEdge("148570021_1100", "148570022_1100", { label: "" });
-//   g.setEdge("148570010_1100", "148570011_1100", { label: "" });
-//   g.setEdge("148570010_1100", "148570010_1200", { label: "" });
-//   g.setEdge("148570020_1100", "148570020_1200", { label: "" });
-//   g.setEdge("148570026_1100", "148570026_1200", { label: "" });
-//   g.setEdge("148570026_1200", "148570011_1200", { label: "" });
-//   g.setEdge("148570010_1200", "148570011_1200", { label: "" });
-//   g.setEdge("148570022_1100", "148570023_1100", { label: "" });
-//   g.setEdge("148570023_1100", "148570023_1200", { label: "" });
-  
-  var svg = d3.select("svg"),
-      inner = svg.select("g");
-  
-  // Set the rankdir
-  g.graph().rankdir = "LR";
-  g.graph().nodesep = 60;
-  
-  // Set up zoom support
-  var zoom = d3.behavior.zoom().on("zoom", function() {
-        inner.attr("transform", "translate(" + d3.event.translate + ")" +
-                                    "scale(" + d3.event.scale + ")");
-      });
-  svg.call(zoom);
-  
-  // Create the renderer
-  var render = new dagreD3.render();
-  
-  
-  // Run the renderer. This is what draws the final graph.
-  render(inner, g);
-  
-  // Center the graph
-  var initialScale = 0.75;
-  zoom
-    .translate([(svg.attr("width") - g.graph().width * initialScale) / 2, 20])
+function processL(g, ns) {
+  ns.forEach((n) => insertNode(g, n));
+  for (let i = 0; i < (ns.length-1); i += 1) {
+    insertEdge(g, ns[i], ns[i+1], decision_list_json[i][i+1])
+  }
+}
+
+function processLL(g, nss) {
+    nss.forEach(ns => processL(g, ns))
+}
+
+console.log(decision_list_json[1][1])
+console.log(processLL(g, decision_list_json))
+
+var svg = d3.select("#diagnosis_box").append("svg").attr("width", 1000).attr("height", 50);
+var inner = svg.append("g");
+
+
+// Set the rankdir
+// g.graph().rankdir = "LR";
+// g.graph().nodesep = 60;
+
+// Set up zoom support
+var zoom = d3.behavior.zoom().on("zoom", function () {
+    inner.attr("transform", "translate(" + d3.event.translate + ")" +
+        "scale(" + d3.event.scale + ")");
+});
+svg.call(zoom);
+
+// // Create the renderer
+// var render = new dagreD3.render();
+
+// // Run the renderer. This is what draws the final graph.
+// render(inner, g);
+
+// // Create the renderer
+// let render = new dagreD3.render();
+// // Draw graph
+// render(inner, g);
+
+// Center the graph
+var initialScale = 0.6;
+zoom
+    .translate([(svg.attr("width") - g.graph().width * initialScale) , 20])
     .scale(initialScale)
     .event(svg);
-  svg.attr('height', g.graph().height * initialScale + 40);
+svg.attr('height', g.graph().height * initialScale + 40);
